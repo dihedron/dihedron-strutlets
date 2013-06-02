31f68021567f2518cdfc01d4ad59d7d0763c39f0
Andrea Funtò <dihedron.dev@gmail.com>
2013-06-02 22:57:17 +0200

    Version 0.11.1: added BACKLOG to keep track or bugs and requirements.

--- src/main/java/org/dihedron/strutlets/Strutlets.java
+++ src/main/java/org/dihedron/strutlets/Strutlets.java
@@ -32,7 +32,7 @@ public final class Strutlets {
 	/**
 	 * The library version.
 	 */
-	public static final String VERSION = "0.11.0";
+	public static final String VERSION = "0.11.1";
 	
 	/**
 	 * The name of the parameter under which the requested action's name is stored
